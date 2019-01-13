9c33c58b52f8d0b239778668faf10584735a3efb
Mils <mils@prontomarketing.com>
2019-01-13 11:51:24 +0700

    Try to print wp_filter :koala:

--- app/public/wp-content/themes/pronto-two/footer.php
+++ app/public/wp-content/themes/pronto-two/footer.php
@@ -2,6 +2,7 @@
         <p> This is footer </p>
         <p><?php do_action( 'print_credits', 'Website by Pronto' ); ?></p>
         <p><?php echo apply_filters( 'replace_pronto_to_me', 'Website by Pronto'); ?></p>
+        <p><?php  //global $wp_filter; var_dump($wp_filter); ?></p>
     </footer>
 
 </body>
